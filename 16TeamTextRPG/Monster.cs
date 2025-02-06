f932de9b35f17534b25f7bd72ece35a68e24543b
leeejinho <86703795+leeejinho@users.noreply.github.com>
2025-02-06 17:42:21 +0900

    [feat] Monster 클래스 변수 생성

--- 16TeamTextRPG/Monster.cs
+++ 16TeamTextRPG/Monster.cs
@@ -8,5 +8,27 @@ namespace _16TeamTextRPG
 {
     internal class Monster
     {
+        string name;
+        bool dead;
+        int level;
+        int maxHealth;
+        int health;
+        int gold;
+        int exp;
+        int attack;
+        int defense;
+
+        public Monster()
+        {
+            name = "";
+            dead = false;
+            level = 0;
+            maxHealth = 0;
+            health = 0;
+            gold = 0;
+            exp = 0;
+            attack = 0;
+            defense = 0;
+        }
     }
 }
