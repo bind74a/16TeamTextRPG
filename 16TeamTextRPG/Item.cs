eff60868e87d958124d9d46bd16798d6826b5dfd
Chanbamx2 <ych7669@naver.com>
2025-02-06 19:08:02 +0900

    [FEAT] 아이템 클래스 추가

--- 16TeamTextRPG/Item.cs
+++ 16TeamTextRPG/Item.cs
@@ -8,6 +8,8 @@ namespace _16TeamTextRPG
 {
     internal class Item
     {
+<<<<<<< Updated upstream
+=======
         public string Name { get; } // 아이템 이름
         public string Option { get; }   // 아이템 옵션
         public string Info { get; } // 아이템 설명
@@ -30,11 +32,51 @@ namespace _16TeamTextRPG
         }
     }
 
-    public class Weapon : Item  // 무기
+    public interface EquipmentItem // 장비 아이템 ( 무기, 방어구, 악세서리에 상속)
+    {
+        public int Power { get; }   // 아이템 공격력
+        public int Defense { get; }   // 아이템 방어력
+        public int Health { get; }   // 아이템 체력
+
+        public void EquipItem(Character player)
+        {
+            IsEquip = IsEquip == false ? true : false;
+            int sign = IsEquip == true ? 1 : -1;
+
+            player.power += Power;
+            player.defense += Defense;
+            player.health += Health;
+        }
+    }
+
+    public class Weapon : Item, EquipmentItem  // 무기
     {
         public Weapon(string name, string option, string info, int price) : base(name, option, info, price)
         {
             Type = "weapon";
+        }
+    }
+    public class Armor : Item, EquipmentItem  // 방어구
+    {
+        public Armor(string name, string option, string info, int price) : base(name, option, info, price)
+        {
+            Type = "armor";
+        }
+    }
+
+    public class Accessory : Item, EquipmentItem  // 악세서리
+    {
+        public Weapon(string name, string option, string info, int price) : base(name, option, info, price)
+        {
+            Type = "accessory";
+        }
+    }
+
+    public class Consumable : Item  // 소모품
+    {
+        public Armor(string name, string option, string info, int price) : base(name, option, info, price)
+        {
+            Type = "consumable";
         }
     }
 
@@ -42,7 +84,13 @@ namespace _16TeamTextRPG
     {
         public List<Item> all = new List<Item>()    // 전체 아이템 리스트
         {
-            new Weapon("낡은 검", "공격력 + 5", "금방이라도 부러질 것 같은 낡은 검 입니다.", 500)
+            new Weapon("낡은 검", "공격력 +2", "쉽게 볼 수 있는 낡은 검 입니다.", 600)
+            new Weapon("청동 도끼", "공격력 +5", "어디선가 사용됐던거 같은 도끼입니다.", 1500)
+            new Weapon("스파르타의 창", "공격력 +7", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2500)
+            new Armor("수련자 갑옷", "방어력 +5", "수련에 도움을 주는 갑옷입니다.", 1000)
+            new Armor("무쇠갑옷", "방어력 +9", "무쇠로 만들어져 튼튼한 갑옷입니다.", 2000)
+            new Armor("스파르타의 갑옷", "방어력 +15", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500)
         };
+>>>>>>> Stashed changes
     }
 }
