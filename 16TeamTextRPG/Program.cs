3073531d7ae90d4440a08c8c643e864f0d158f56
greenmetamong <youngjaeda@naver.com>
2025-02-06 17:45:04 +0900

    [feat]게임시작 화면이랑이랑 상태 화면 작성

--- 16TeamTextRPG/Program.cs
+++ 16TeamTextRPG/Program.cs
@@ -8,4 +8,101 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    //게임 매니저 클래스
+    class GameManager
+    {
+        Player player;
+
+        public GameManager()
+        {
+            //새로운 플레이어를 생성
+            player = new Player(1, "메타몽", "전사",  10, 5, 100, 1500);
+
+            //아이템 리스트 생성
+        }
+
+
+        public void MainScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
+            Thread.Sleep(500);
+            Console.WriteLine();
+            Console.WriteLine("1. 거울로 내 상태 보기");
+            Console.WriteLine("2. 가방 열어 인벤토리 보기");
+            Console.WriteLine("3. 돈 쓰러 상점 가기");
+            Console.WriteLine("4. 던전 들어가기");
+            Console.WriteLine();
+            Thread.Sleep(500);
+
+            //숫자를 입력하면 해당하는 화면으로 이동
+            while (true)
+            {
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+                Console.ReadLine();
+
+                int input = ConsoleUtility.GetInput(1, 5);
+
+                switch (input)
+                {
+                    case 1:
+                        StatusScreen();
+                        break;
+                    case 2:
+                        InventoryScreen();
+                        break;
+                    case 3:
+                        ShopScreen();
+                        break;
+                    case 4:
+                        DunguenScreen();
+                        break;
+                }
+
+                Console.WriteLine("잘못된 입력입니다. 다시 입력해 주세요.");
+            }
+        }
+
+        //캐릭터 선택창
+        public void StatusScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("【상태창】");
+            Console.WriteLine("이곳은 캐릭터의 정보를 볼 수 있습니다.");
+            Console.WriteLine();
+            Thread.Sleep(500);
+
+            player.StatusDisplay(); //플레이어 스탯시트 표시
+
+
+            Thread.Sleep(500); 
+            Console.WriteLine();
+            Console.WriteLine("선택창)");
+            Console.WriteLine("0) 나가기");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+                Console.ReadLine();
+
+                int input = int.Parse(Console.ReadLine());
+
+                if (input == 0)
+                {
+                    MainScene();
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 다시 입력해 주세요.");
+                    continue;
+                }
+
+            }
+        }
+    }
 }
