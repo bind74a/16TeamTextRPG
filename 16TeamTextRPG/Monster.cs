96baca3e214819dafb419d6d43b8de3a8a209a3c
leeejinho <86703795+leeejinho@users.noreply.github.com>
2025-02-06 17:43:03 +0900

    [feat] Monster 공격 함수 추가

--- 16TeamTextRPG/Monster.cs
+++ 16TeamTextRPG/Monster.cs
@@ -30,5 +30,31 @@ namespace _16TeamTextRPG
             attack = 0;
             defense = 0;
         }
+
+        public void Attack(Player player)
+        {
+            Random rand = new Random();
+
+            // 공격력의 10% 오차 (오차가 소수점이라면 올림처리);
+            int min = attack - (int)Math.Ceiling(attack * 0.1f);
+            int max = attack + (int)Math.Ceiling(attack * 0.1f);
+
+            // 최종 공격력
+            int final = rand.Next(min, max + 1);
+
+            player.health -= final;
+            if (player.health <= 0)
+            {
+                player.dead = true;
+                player.health = 0;
+            }
+
+            // Consol UI
+            Console.WriteLine($"Lv.{level} {name}의 공격!"); // Lv.2 미니언의 공격!
+            Console.WriteLine($"{player.name}을(를) 맞췄습니다. [데미지: {final}]\n"); // Chad 을(를) 맞췄습니다. [데미지: 6]
+
+            Console.WriteLine($"Lv.{player.level} {player.name}"); // Lv.1 Chad
+            Console.WriteLine($"HP {player.health} -> {player.health}"); // HP 100 -> 94
+        }
     }
 }
