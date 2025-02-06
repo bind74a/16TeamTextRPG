9e4c4831564a3d92a8ca39e124ffcdccf45c0774
subin <subin24890@gmail.com>
2025-02-06 16:00:46 +0900

    Initial commit

---
+++ 16TeamTextRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _16TeamTextRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
