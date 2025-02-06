5fce1c7ac5a99cb76b0f590987a26243a93378e9
subin <subin24890@gmail.com>
2025-02-06 20:35:43 +0900

    [style] 코드 공간 삭제

--- 16TeamTextRPG/Stage.cs
+++ 16TeamTextRPG/Stage.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 
 namespace _16TeamTextRPG
+{
+    internal class Stage
     {
-        internal class Stage
-        {
             //구현할것 : 전투신 전체의 로직
-            static List<Monster> monsters = new List<Monster>();
+         static List<Monster> monsters = new List<Monster>();
 
-            static Random Random = new Random();
+         static Random Random = new Random();
 
 
-            public List<Monster> SummonMonster()//몬스터 소환
-            {
+         public List<Monster> SummonMonster()//몬스터 소환
+         {
                 //1.몬스터 클래스에서 몬스터 정보받고 그정보로 몬스터 랜덤성으로 1~4마리 소환
                 List<Monster> list = new List<Monster>();
 
@@ -29,11 +29,10 @@ namespace _16TeamTextRPG
                     list.Add(monsters[randomMon]);
                 }
                 return list;
-            }
+         }
 
-            public void Battlefield()
-            {
-
+         public void Battlefield()
+         {
                 //소환됀 객체들을 필드위에 뛰운다 그리고 필드 몬스터 리스트화?
                 Console.WriteLine("Battle!!");
                 Console.WriteLine();
@@ -41,9 +40,9 @@ namespace _16TeamTextRPG
 
                 Console.WriteLine();
                 Console.WriteLine("[내정보]");
-            }
+         }
             //2.공격하기 선택창을 추가하여 공격시 전투 계산 메소드 작동
             //3.전투가 끝날시 남아있는 현재체력과 현재 스탯표시
-        }
     }
+}
 
