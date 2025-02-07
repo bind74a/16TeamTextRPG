5f1a5404c993075711a3d0c2e097953905897fb4
DasaemKim <kds000203@gmail.com>
2025-02-07 12:04:36 +0900

    (faet) 기능 추가

--- 16TeamTextRPG/Program.cs
+++ 16TeamTextRPG/Program.cs
@@ -1,14 +1,5 @@
 ﻿namespace _16TeamTextRPG
 {
-    internal class Program
-    {
-        //깃허브 테스트 2
-        static void Main(string[] args)
-        {
-            
-        }
-    }
-
     //게임 매니저 클래스
     class GameManager
     {
@@ -22,11 +13,41 @@
             //아이템 리스트 생성
         }
 
+        private string name; //    _ 
+        //                         ㅣ->  외부에서 직접 호출할 필요 없어서 Private 사용     
+        private Job plyaerJob; //  -
+
+        enum Job
+        {
+            Warrior = 1, // 1을 안쓰면 0부터 시작하기에 1부터 시작하도록 값을 부여
+            Mage,
+            Archer,
+            Thief,
+        }
+
+        private Dictionary<Job, string> jobNames = new Dictionary<Job, string>() //enum에 제시된 영어를 한글화
+        {
+            {Job.Warrior, "전사"},
+            {Job.Mage, "마법사"},
+            {Job.Archer, "궁수"},
+            {Job.Thief, "도적"}
+        };
 
         public void MainScreen()
         {
             Console.Clear();
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("당신의 이름을 적어주세요.");
+            string name = Console.ReadLine();
+
+            playJob = SelcetJob()
+
+            Console.WriteLine();
+            Console.WriteLine("1. 전사");
+            Console.WriteLine("2. 마법사");
+            Console.WriteLine("3. 아처");
+            Console.WriteLine("4. 마법사");
+            string job = Console.ReadLine();    
             Console.WriteLine("이제 전투를 시작할 수 있습니다.");
             Thread.Sleep(500);
             Console.WriteLine();
@@ -63,6 +84,29 @@
                 }
 
                 Console.WriteLine("잘못된 입력입니다. 다시 입력해 주세요.");
+            }
+        }
+
+        private Job SelectJob()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n하고 싶은 직업을 골라주세요.");
+                Console.WriteLine("1. 전사");
+                Console.WriteLine("2. 마법사");
+                Console.WriteLine("3. 궁수");
+                Console.WriteLine("4. 도적");
+                Console.Write("번호 입력해주세요\n>>");
+
+                if (int.TryParse(Console.ReadLine(), out int choice) && Enum.IsDefined(typeof(Job), choice))
+                {
+                    return (Job)choice;
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 1~4 사이의 숫자를 입력하세요.");
+                }
+
             }
         }
 
